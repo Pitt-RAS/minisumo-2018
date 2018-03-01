d91f0480c10b159ce971ce2afb476afb5975d82b
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-02-28 23:00:55 -0500

    Added inline usage documentation

--- libmotor/Motor.cpp
+++ libmotor/Motor.cpp
@@ -1,30 +1,48 @@
 #include "Motor.h"
-
+/*
+ * Creating a motor instantiates four integers, *and that is all*
+ * in1, in2 -> digital output pin connecting to (A/B)IN(1/2) of the driver 
+ * pwm -> analog output pin connecting to PWM(A/B)
+ * stby -> digital output bridged to standby of motor driver
+ */
 Motor::Motor( int in1, int in2, int pwm, int stby ){
   this->in1 = in1;
   this->in2 = in2;
   this->pwm = pwm;
   this->stby = stby;
 }
-
+/*
+ * Activates the motor clockwise, where speed is reflected as an integer 'pwmv'
+ * between 0 and 255. 255 is max, 0 is none.
+ */
 void Motor::clockwise( int pwmv ){
   digitalWrite( in1, HIGH );
   digitalWrite( in2, LOW );
   analogWrite( pwm, pwmv );
   digitalWrite( stby, HIGH );
 }
+/*
+ * Activates the motor anticlockwise, speed is reflected by 'pwmv', and integer 
+ * between 0 and 255. 255 is max, 0 is none.
+ */
 void Motor::anticlockwise( int pwmv ){
   digitalWrite( in1, LOW );
   digitalWrite( in2, HIGH );
   analogWrite( pwm, pwmv );
   digitalWrite( stby, HIGH );
 }
+/*
+ * Don't use this, we don't know what it does yet.
+ */
 void Motor::short_brake( int pwmv ){
   digitalWrite( in1, HIGH );
   digitalWrite( in2, HIGH );
   analogWrite( pwm, pwmv );
   digitalWrite( stby, HIGH );    
 }
+/*
+ * Don't use this, we don't know what it does yet.
+ */
 void Motor::cold_stop( int pwmv ){
   digitalWrite( in1, HIGH );
   digitalWrite( in2, HIGH );
