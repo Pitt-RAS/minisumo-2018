d8e18c3f3e29c1bfdd2afb49e081a9720aca49ab
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-02-28 22:29:35 -0500

    Created a motor test
    The motor test gradually increases the PWM for both motors and then stops.

--- driver_test/driver_test.ino
+++ driver_test/driver_test.ino
@@ -4,25 +4,24 @@ int BIN1 = 3;
 int STBY = 4;
 int AIN1 = 5;
 int AIN2 = 6;
-int PWMA = 7;
-int PWMB = 8;
-#include <Motor.h>
+int PWMA = 10;
+int PWMB = 9;
+
 Motor *a = new Motor( AIN1, AIN2, 10, STBY );
+Motor *b = new Motor( BIN1, BIN2, 9 , STBY );
 void setup() {
   int i;
   for ( i = 2; i < 13; i++ ){
     pinMode( i, OUTPUT );
   }
-  pinMode( 10, OUTPUT );
   Serial.begin(9600);
 }
 void loop() {
-  while ( 1 ){
-    int x;
-    for ( x = 0; x <= 255; x++){
-      delay(100);
-      Serial.println(x);
-      a -> clockwise( x );
-    }
+  int x;
+  for ( x = 0; x <= 255; x++){
+    delay(100);
+    Serial.println(x);
+    a -> clockwise( x );
+    b -> clockwise( x );
   }
 }
