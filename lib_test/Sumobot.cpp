8fd2e5cc61dd34141cf7a56e8ac1b24f0fca8613
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-03-04 01:13:56 -0500

    Inline documentation

--- lib_test/Sumobot.cpp
+++ lib_test/Sumobot.cpp
@@ -1,6 +1,8 @@
 #include "Sumobot.h"
 /*
  * Eventually we will need sensors, encoders, etc.
+ * For now, this constructor initializes a robot based off its motors (pre-
+ * initialized before this, as well.)
  */
 Sumobot::Sumobot( Motor * lf, Motor * rf, Motor * lb, Motor * rb ){
   this -> lf = lf;
@@ -14,12 +16,20 @@ void Sumobot::rotate_clockwise( int deg ){
 void Sumobot::rotate_anticlockwise( int deg ){
   
 }
+/*
+ * Component-required setup function. Triggers setup function of each motor.
+ */
 void Sumobot::setup( ){
   this -> lf -> setup( );
   this -> rf -> setup( );
   this -> lb -> setup( );
   this -> rb -> setup( );
 }
+/*
+ * Returns an array containing a pointer to an array for each motor. The 
+ * arrays pointed to will contain the pins in use by that motor, in respective
+ * order ofL lf, rf, lb, rb.
+ */
 int* Sumobot::get_pins( ){
   int res[ 4 ] = {  this -> lf -> get_pins( ), 
                     this -> rf -> get_pins( ), 
