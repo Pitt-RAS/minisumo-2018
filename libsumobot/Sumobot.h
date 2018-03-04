a438e8a706180439c8d10f186781289b3ae463f6
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-03-04 00:12:55 -0500

    Fixed headers to react properly to file names

--- libsumobot/Sumobot.h
+++ libsumobot/Sumobot.h
@@ -1,16 +1,18 @@
 #ifndef SUMOBOT_H
 #define SUMOBOT_H
 #include <Arduino.h>
-#inclde <Motor.h>
-class Sumobot{
+#include "Motor.h"
+#include "Component.h"
+class Sumobot: public Component {
 private:
   Motor lf;
   Motor lb;
   Motor rf;
   Motor rb;
 public: 
-  Robot( Motor lf, Motor rf, Motor lb, Motor rb );
-  ~Robot( );
-  
+  Sumobot( Motor lf, Motor rf, Motor lb, Motor rb );
+  ~Sumobot( );
+  void setup ( );
+  int* get_pins( );
 };
 #endif
