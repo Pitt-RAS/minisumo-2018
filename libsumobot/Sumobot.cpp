32b281d659fb8c0d35baedef391ecdac9f4fc84d
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-03-04 00:40:48 -0500

    Libsumobot now constructs without errors (for now)

--- libsumobot/Sumobot.cpp
+++ libsumobot/Sumobot.cpp
@@ -2,17 +2,23 @@
 /*
  * Eventually we will need sensors, encoders, etc.
  */
-Sumobot::Sumobot( Motor lf, Motor rf, Motor lb, Motor rb ){
+Sumobot::Sumobot( Motor * lf, Motor * rf, Motor * lb, Motor * rb ){
   this -> lf = lf;
   this -> rf = rf;
   this -> lb = lb;
   this -> rb = rb;
 }
+void Sumobot::rotate_clockwise( int deg ){
+  
+}
+void Sumobot::rotate_anticlockwise( int deg ){
+  
+}
 void Sumobot::setup( ){
-  this -> lf.setup( );
-  this -> rf.setup( );
-  this -> lb.setup( );
-  this -> rb.setup( );
+  this -> lf -> setup( );
+  this -> rf -> setup( );
+  this -> lb -> setup( );
+  this -> rb -> setup( );
 }
 int* Sumobot::get_pins( ){
   int res[ 4 ];
--- libsumobot/Sumobot.h
+++ libsumobot/Sumobot.h
@@ -5,14 +5,16 @@
 #include "Component.h"
 class Sumobot: public Component {
 private:
-  Motor lf;
-  Motor lb;
-  Motor rf;
-  Motor rb;
+  Motor * lf;
+  Motor * lb;
+  Motor * rf;
+  Motor * rb;
 public: 
-  Sumobot( Motor lf, Motor rf, Motor lb, Motor rb );
+  Sumobot( Motor * lf, Motor * rf, Motor * lb, Motor * rb );
   ~Sumobot( );
   void setup ( );
   int* get_pins( );
+  void rotate_clockwise( int deg );
+  void rotate_anticlockwise( int deg );
 };
 #endif
