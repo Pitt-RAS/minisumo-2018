2382e418c33c7caf1af0df42776e66ddaa8b286b
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-03-04 00:12:03 -0500

    Default destructor and abstract methods implemented

--- libsumobot/Motor.h
+++ libsumobot/Motor.h
@@ -1,16 +1,20 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 #include <Arduino.h>
-class Motor{
+#include "Component.h"
+class Motor: public Component{
 private:
   int in1, in2, pwm, stby;
 public:
+  Motor( );
   Motor( int in1, int in2, int pwm, int stby );
   ~Motor( );
   void clockwise( int pwmv );
   void anticlockwise( int pwmv );
   void short_brake( int pwmv );
-  void cold_stop( int pwmv );
+  void cold_stop( );
   int* get_pins( );
+  void setup( );
+  /*int get_velocity( );*/
 };
 #endif
