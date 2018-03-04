4f28b2e12ed11a0af40e383b10312ac2ba8e7bd7
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-03-04 00:12:34 -0500

    Created a Sumobot class

---
+++ libsumobot/Sumobot.cpp
@@ -0,0 +1,20 @@
+#include "Sumobot.h"
+/*
+ * Eventually we will need sensors, encoders, etc.
+ */
+Sumobot::Sumobot( Motor lf, Motor rf, Motor lb, Motor rb ){
+  this -> lf = lf;
+  this -> rf = rf;
+  this -> lb = lb;
+  this -> rb = rb;
+}
+void Sumobot::setup( ){
+  this -> lf.setup( );
+  this -> rf.setup( );
+  this -> lb.setup( );
+  this -> rb.setup( );
+}
+int* Sumobot::get_pins( ){
+  int res[ 4 ];
+  return res;
+}
