21f881328369f63de03f727cfab2c3db24fcaa40
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-03-28 21:26:16 -0400

    Got rid of stuff involving signals because that's above our paygrade

---
+++ ir_test/ircode.ino
@@ -0,0 +1,25 @@
+#define irLedPin 0
+#define irSensorPin 1
+
+int irRead(int readPin, int triggerPin);
+
+void setup()
+{
+  pinMode(irSensorPin, INPUT_PULLUP);
+  pinMode(irLedPin, OUTPUT);
+  Serial.begin(9600); 
+  tone(0, 48000);
+
+}
+
+void loop()
+{  
+  Serial.println(irRead(irSensorPin, irLedPin));
+  delay(100);
+}
+
+int irRead(int readPin, int triggerPin)
+{
+  /*This is all there is to it*/
+  return digitalRead(readPin);
+}
