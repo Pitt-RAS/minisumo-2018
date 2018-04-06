209b1efede5e91612ae97d307c1793fb9422346d
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-04-06 19:12:34 -0400

    Update PhotoSensor.h

--- PhotoSensor.h
+++ PhotoSensor.h
@@ -2,7 +2,7 @@
 #define PHOTOSENSOR_H
 #include <Arduino.h>
 #include "Component.h"
-#define BOUNDARY_SENSE_THRESHOLD 200
+#define BOUNDARY_SENSE_THRESHOLD 400
 class PhotoSensor: public Component{
 private: 
   int read_pin;
