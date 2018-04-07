ebeccdb40462f17a17b05441062227cb67d7aa71
Connor Berry <34040804+cdb84@users.noreply.github.com>
2018-04-07 03:12:44 -0400

    Note to dead battery laptop stand-in: change rotational pwm

--- Sumobot.h
+++ Sumobot.h
@@ -9,13 +9,13 @@
 
 #define TEST_PWM 250
 #define DEFAULT_PWM 125 //formerly 150
-#define ROTATIONAL_PWM 255 //formerly 100
+#define ROTATIONAL_PWM 150 //formerly 100
 #define MICRO_ADJUST_PWM 35 //formery 50
 #define MAX_PWM 255
 #define JETTISON_RUN_DELAY 100
 #define BRAKE_GRACE_DELAY 50
 #define BOUND_TICK_DELAY 500
-#define ROTATE_TICK_DELAY 250
+#define ROTATE_TICK_DELAY 75
 #define SEE_NOTHING_DELAY 50
 #define SIDE_BOUNDARY_DELAY 750
 class Sumobot: public Component {
